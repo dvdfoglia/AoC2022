e469b5cbfebe28e8c62c3eb6377148dbeae68d1e
Davide Fogliarini <davide.fogliarini@gmail.com>
2022-12-03 18:59:53 +0100

    day1 resolved

--- day1/day1.py
+++ day1/day1.py
@@ -3,14 +3,21 @@ d="day1"
 dname = os.path.dirname(os.path.abspath(__file__))
 
 def read_input():
-    f=dname+"/"+d+".txt"
+    f = dname+"/"+d+".txt"
     input = open(f,'r')
-    i=input.read()
+    i = input.read().splitlines()
     input.close()
     return i
-print(read_input())
-#c=0
-#for t in read_input():
-#    print(t)
-#    c=c+1
-#    print(c)
+elf=0
+tot_cal=[0]
+data=read_input()
+for t in data:
+    #print(elf)
+    if t == "":
+        elf=elf+1
+        tot_cal.append(0)
+    else:
+        cal=int(t)
+        tot_cal[elf]=tot_cal[elf]+cal
+
+print(max(tot_cal))
