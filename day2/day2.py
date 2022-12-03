3378cfef19712d5cf76d009fa08cfd941faea4a3
Davide Fogliarini <davide.fogliarini@gmail.com>
2022-12-03 19:28:14 +0100

    day2 starting

--- day2/day2.py
+++ day2/day2.py
@@ -2,26 +2,28 @@ import os
 d="day2"
 dname = os.path.dirname(os.path.abspath(__file__))
 
+paper=2
+scissors=3
+lost=0
+draw=3
+win=6
+r1="A"
+p1="B"
+s1="C"
+r1="X"
+p1="Y"
+s1="Z"
+
+
 def read_input():
     f = dname+"/"+d+".txt"
     input = open(f,'r')
     i = input.read().splitlines()
     input.close()
     return i
-elf=0
-tot_cal=[0]
-tot_cal3=0
-data=read_input()
-for t in data:
-    #print(elf)
-    if t == "":
-        elf=elf+1
-        tot_cal.append(0)
-    else:
-        cal=int(t)
-        tot_cal[elf]=tot_cal[elf]+cal
 
-tot_cal.sort(reverse=True)
-for i in range(0,3):
-    tot_cal3=tot_cal3+tot_cal[i]
-print(tot_cal3)
+data=read_input()
+
+for i in data:
+    play=i.split()
+    print(play)
