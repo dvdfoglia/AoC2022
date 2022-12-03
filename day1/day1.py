86e38aab9d45822edddd1a01b90b6ad66f3a6d2b
Davide Fogliarini <davide.fogliarini@gmail.com>
2022-12-03 18:09:56 +0100

    modified:   day1/day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,16 @@
+import os
 d="day1"
-f=d+"/"+d+".txt"
-input = open(f,'r')
-print(input.read())
+dname = os.path.dirname(os.path.abspath(__file__))
+
+def read_input():
+    f=dname+"/"+d+".txt"
+    input = open(f,'r')
+    i=input.read()
+    input.close()
+    return i
+print(read_input())
+#c=0
+#for t in read_input():
+#    print(t)
+#    c=c+1
+#    print(c)
