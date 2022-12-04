310e48accb7956f4abb434a3f26216981d42d324
Davide Fogliarini <davide.fogliarini@gmail.com>
2022-12-04 22:36:53 +0100

    day4 completed

---
+++ day4/day4.py
@@ -0,0 +1,32 @@
+import os
+d="day4"
+dname = os.path.dirname(os.path.abspath(__file__))
+
+def read_input():
+    f = dname+"/"+d+".txt"
+    input = open(f,'r')
+    i = input.read().splitlines()
+    input.close()
+    return i
+
+def overlap_all(data):
+    t1=0
+    t2=0
+    t3=len(data)
+
+    for i in data:
+        g=(list(map(int,i.replace("-",",").split(sep=","))))
+        if (g[0] <= g[2] and g[1] >= g[3]) or (g[2] <= g[0]) and (g[3] >= g[1]):
+            t1+=1
+        if (g[0]>g[2]):
+            if (g[0]>g[3]):
+                t2+=1
+        elif g[0]<g[2]:
+            if g[1]<g[2]:
+                t2+=1
+    return t1,t2,t3
+
+
+res=overlap_all(read_input())
+print(res[0])
+print(res[2]-res[1])
